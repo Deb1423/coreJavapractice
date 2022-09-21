39d12fb9548d681be296a7399be02fd7be60af56
Deb1423 <113019294+Deb1423@users.noreply.github.com>
2022-09-21 20:26:12 +0530

    Add files via upload

---
+++ 13sep/WhileDemo.java
@@ -0,0 +1,53 @@
+/*
+Program :While Demo 
+@author: DEBJIT TALUKDER
+@Date: 13sep,2022.
+*/
+
+class WhileDemo
+{
+	static void bulbStatus(int status)
+	{
+		
+	    	while(status>=10)// entry controlled loop
+	    	{
+				
+		    	System.out.println("Bulb is ON" + " ----Durability :"+status);
+				status-=5;
+					    	
+		    }
+			
+			if(status<10)
+			System.out.println("Bulb is OFF");
+			
+		
+	}
+	
+	static void bulbStatusDo(int status)
+	{
+		
+	    	do   // exit controlled loop
+	    	{
+				
+		    	System.out.println("Bulb is ON" + " ----Durability :"+status);
+				status-=5;
+					    	
+		    }while(status>=10);
+			
+			if(status<10)
+			System.out.println("Bulb is OFF");
+			
+		
+	}
+	
+	
+	
+		
+   public static void main(String... args)
+   {
+	   int durability =Integer.parseInt(args[0]);
+	 bulbStatusDo(durability); 
+   }
+
+
+}
